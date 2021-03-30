5287b7ea89a3d35a3f263c1034d8ae0eb9481f85
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-03-30 11:12:03 +0900

    2021 03 30 //Intent

---
+++ app/s20210330_1/src/main/java/com/example/s20210330_1/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.s20210330_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Log.i("chiffon95", "Landscape");
+        }else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Log.i("chiffon95", "Portrait");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("chiffon95", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("chiffon95", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("chiffon95", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("chiffon95", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("chiffon95", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("chiffon95", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("chiffon95", "onDestroy");
+    }
+
+    public void onClick(View view){
+        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(i);
+    }
+}
