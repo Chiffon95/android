4f57a06e4cc7c0645b2fa8fcc176275a4abfbc56
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-04-01 14:29:19 +0900

    2021-04-01 thread ~ing2

--- app/s20210401_handler_runnable/src/main/java/com/example/s20210401_handler_runnable/MainActivity.java
+++ app/s20210401_handler_runnable/src/main/java/com/example/s20210401_handler_runnable/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.s20210401_handler_runnable;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler = new Handler();
     TextView tv;
     Button btn;
     int cnt = 0;
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView)findViewById(R.id.tv_text);
     }
 
-    void onClickStart3(View v){
+    public void onClickStart3(View v){
 
         cnt = 0;
         tv.setText("Count :" + cnt);
@@ -29,12 +32,27 @@ public class MainActivity extends AppCompatActivity {
         Thread th_cnt = new Thread("Count Thread"){
             @Override
             public void run() {
-                super.run();
+                for(int i = 0; i < 5; i++){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    cnt++;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText(cnt + "초");
+                        }
+                    });
+                    Log.i("chiffon95", "Count : " + cnt);
+                }
             }
         };
+        th_cnt.start();
     }
 
-    void onClickStop3(View v){
+    public void onClickStop3(View v){
 
     }
 }
