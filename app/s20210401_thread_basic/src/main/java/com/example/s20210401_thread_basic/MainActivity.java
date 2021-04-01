ace74703f2e1a622aa2946cf87406769b70e0e88
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-04-01 11:29:44 +0900

    2021-04-01 thread ~ing

---
+++ app/s20210401_thread_basic/src/main/java/com/example/s20210401_thread_basic/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.s20210401_thread_basic;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView tv;
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStart(View v) {
+        count = 0;
+
+        for(int i = 0; i < 5; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            count++;
+            tv.setText("count : " + count);
+            Log.i("chiffon95", "count : " + count);
+        }
+    }
+
+    public void onClickStop(View v) {
+        Toast.makeText(this, "Count : " + count,
+                Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/s20210401_thread_handler/src/main/java/com/example/s20210401_thread_handler/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.s20210401_thread_handler;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int WHAT_HANDLER_MSG_COUNT = 1;
+    TextView tv;
+    Button btn;
+    int count;
+    Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if(msg.what == WHAT_HANDLER_MSG_COUNT && msg.arg1 < 5){
+                count++;
+                tv.setText(count + "초");
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv_text);
+
+    }
+
+    public void onClickStart(View v) {
+        count = 0;
+
+        Thread th_count = new Thread(){
+            @Override
+            public void run() {
+
+                for(int i = 0; i < 5; i++){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Message msg = handler.obtainMessage(WHAT_HANDLER_MSG_COUNT, count);
+                    handler.sendMessage(msg);
+                    Log.i("chiffon95", " onClickStart Run count : " + count);
+                }
+            }
+        };
+        th_count.start();
+    }
+
+    public void onClickStop(View v) {
+
+        handler.removeMessages(WHAT_HANDLER_MSG_COUNT);
+        Toast.makeText(this, "onClickStop Count : " + (count + 1),
+                Toast.LENGTH_LONG).show();
+    }
+}
